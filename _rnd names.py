4379b1e3243520bc81d4f23221abfc0948f57725
clarence <clarence.lewis@gmail.com>
2026-03-29 22:31:36 -0400

    bare meat on da bonz

--- _rnd names.py
+++ _rnd names.py
@@ -1,14 +1,14 @@
 from random import choice
 n=[]
 #for i in 
-a=['tommy','pamela','james']
+a=['tommy','pamela','james','mike']
 b=['henderson','johnson','lewis','baxter','mason','akbar']
-for i in range(0,7):
+for i in range(0,5):
     rname=(choice(a),choice(b))
     n.append (rname)
-    print (rname)
-    print ((n))
-    print(set(n))
+   # print (rname)
+    #print ((n))
+    print((n))
     h=n.count(rname)
 print ('number of unique names in list is  ',h)
 
